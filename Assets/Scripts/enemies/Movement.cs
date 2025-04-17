c4f615bd99d78d2853a9b332f999cf126417f06a
MacCready <paulcharbel@proton.me>
2025-04-17 10:28:58 +0200

    enemy spawn and attack

--- Assets/Scripts/enemies/Movement.cs
+++ Assets/Scripts/enemies/Movement.cs
@@ -6,14 +6,14 @@ public class Movement : MonoBehaviour
     public float movementSpeed = 1.0f; 
 
     public GameObject hq;
-    public GameObject self; 
+    public GameObject self;
     void Start()
     {
         hq = GameObject.FindWithTag("HQ");
-        self = GameObject.FindWithTag("Enemy");
     }
 
     void Update()
         {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(hq.transform.position.x,transform.position.y, transform.position.z), movementSpeed * Time.deltaTime);
         }
     }
--- Assets/Scripts/enemies/Spawn.cs
+++ Assets/Scripts/enemies/Spawn.cs
@@ -6,20 +6,22 @@ namespace enemies
         public GameObject spawnPoint;
         private float delay;
         private int amountSpawned;
+        private int maxAmountSpawned = 5;
+        public GameObject Orc;
         
         private void Start()
         {
             delay = 3;
             amountSpawned = 0;
-            spawnPoint = GetComponent<>();
+            spawnPoint = GameObject.FindWithTag("SpawnPoint");
             
         }
 
         void Update()
         {
-            if (delay >= 1){
-                GameObject temp = Instantiate(spawnPoint, new Vector3(0*5,0 ,Random.Range(-10, 10)), Quaternion.identity);
-                temp.name = "Orc" + amountSpawned.ToString();
+            if (delay >= 1 && maxAmountSpawned > amountSpawned){
+                GameObject temp = Instantiate(Orc, new Vector3(0*5,0 ,Random.Range(-10, 10)), Quaternion.identity);
+                temp.name = "Orc" + amountSpawned;
                 delay = 0;
                 amountSpawned++;
                 print(delay);
