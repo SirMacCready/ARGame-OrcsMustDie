a60ff3b7a8f581d330f828db83d932c365cf0a47
jiad-saegus <jiad.abdul@saegus.com>
2025-04-18 11:06:55 +0200

    Healthbar

--- Assets/Scripts/HQ/HealthBar.cs
+++ Assets/Scripts/HQ/HealthBar.cs
@@ -6,7 +6,10 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public int health = 100; // Health value, can be set to any value between 0 and 100
+    private int maxHealth = 100; // Maximum health value
+    private bool gameLost = false; // Flag to check if the game is lost
     public Slider slider;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -27,9 +30,18 @@ public class HealthBar : MonoBehaviour
     public int GetHealth(){
             return health; // Return the current health value
         }
+    public int GetMaxHealth(){
+            return maxHealth; // Return the maximum health value
+    }
     public void SetHealth(int health){
         this.health = health;
         slider.value = health; // Update the slider value to reflect the current health
         Debug.Log("Health set to: " + health);
+    }
+    public void SetGameLost(bool dead){
+        gameLost = dead;
+    }
+    public bool GetGameLost(){
+        return gameLost;
     }
 }
